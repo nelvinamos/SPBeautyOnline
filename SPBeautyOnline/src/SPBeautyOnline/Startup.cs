f38690dc3fae865f6e1465a963407a3724ac46d0
Hmar Tlangval <52002854+hmartlangval@users.noreply.github.com>
2020-12-03 16:28:06 +0530

    Initial Commit - New Project

---
+++ SPBeautyOnline/src/SPBeautyOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPBeautyOnline.Startup))]
+namespace SPBeautyOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
